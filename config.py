66c892f9b69874af6841f7a6b825e6325483e738
Abdodbjjg <133290416+Abdodbjjg@users.noreply.github.com>
2023-05-12 03:27:56 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,19 +5,19 @@ admins = {}
 load_dotenv()
 
 # client vars
-API_ID = int(getenv("API_ID", "7452578"))
-API_HASH = getenv("API_HASH", "061d67ee8eed9368c5cadabb4aa21efc")
-BOT_TOKEN = getenv("BOT_TOKEN", "5385370603:AAHjSbZFL1OLl_uOm3v8lS0iKSYhRJS_bKs")
-SESSION_NAME = getenv("SESSION_NAME", "AgAGFRW8AN9biZhAIB_X7rubbTCwNtc48oa-geXlaVsQbV9M1emAEV_iGPw_W-lNP1HGbOuQgF9NAUIxlp8-6rgN16-7Upuhst2wCxewyx0VP7Cxb5k_9EXap6_xeU2v4So9RZMt95XYxhGc4hWO_ikd9P46iHLMjXW8GjQAnGvY-bIrM6XqIXF1yeC3vixEa8knOHGkedZU3FYyLhEILy8TV6eH5jOGvsyw5o7xFElb2zZId_vlYO--B3xSWIMUR6zyH8wozehe9tIn0jZptYjY1rqWOMoYS9wtqvBThVazL60nrgL-KdikJC9beDJLLR0C-wzuAtARPCZ-JK6oG-gVAAAAAUM3evYA")
+API_ID = int(getenv("API_ID", "21967614"))
+API_HASH = getenv("API_HASH", "0925fb18d5910caa9782e317b7a2149a")
+BOT_TOKEN = getenv("BOT_TOKEN", "6170338903:AAEYiPBOzLsuOZv5yCACadK9C11a22oYyQI")
+SESSION_NAME = getenv("SESSION_NAME", "BABUItQDOnJCKhDYgOjsM0ba0ln6QQ29fnM7qJRMHJGwVPRSbA06g9QA5a4wE1MwaQSng1uzpFAv9bInWQdTjnS62NHrpMGqOsavqv6BkO9vuwBYXuZ60j64GuzHsDYxRsOUlY9dZ6YTzrggHS51PuOcVHeE5zVipn1l_y7pTFLDFfROGPy07NBISTK43cphV2j9Mi6k0igubItg9MBZqvOGHMbMYxmlV0dQQQrLRHosS7oTxBpJheCYQSPGkpvOkt8Z0auZ96H3y8erGUO0--6qzAKweL3vC8uAQ3EsFySrXiUf-boskkj2HUZCMl4CMrNtIfHNYZ8W3AWdsVnGf-IxAAAAAUYhxK4A")
 
 # mandatory vars
-OWNER_USERNAME = getenv("OWNER_USERNAME", "rr8r9")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "BotConfectionary")
 ALIVE_NAME = getenv("ALIVE_NAME", "song")
-BOT_USERNAME = getenv("BOT_USERNAME", "W7MBoT")
+BOT_USERNAME = getenv("BOT_USERNAME", "BotConfectionarybot")
 UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/STKR2/2004")
 UPSTREAM_BRANCH = getenv("UPSTREM_BRANCH", "main")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "60"))
-GROUP_SUPPORT = getenv("GROUP_SUPPORT", "rr8r9")
+GROUP_SUPPORT = getenv("GROUP_SUPPORT", "ggkauyggg")
 UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "xl444")
 
 # database, decorators, handlers mandatory vars
